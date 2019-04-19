e1c2fc6b3d2faa2c697e43717b91a000e5190825
Murphy <Murphy@DESKTOP-FVG5P00>
2019-04-19 11:15:27 +0800

    测试第二次提交

--- src/com/basic/test_20190414/Test.java
+++ src/com/basic/test_20190414/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		Parent p = new Son(); // 3.父类的引用指向子类的对象
 		Parent p1 = new Daughter(); // 3.父类的引用指向子类的对象
-		System.out.println(p + ":" + "aa" + "/r/n" + p1 + ":" + "bb");
+		System.out.println(p + ":" + "aa" + "；" + p1 + ":" + "bb");
 	}
 
 }
